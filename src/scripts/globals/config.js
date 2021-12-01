c3ed16ff653380be73b52523fcac0a496bdf3c2c
mrsyafapri <rizkisyafapri@gmail.com>
2021-12-01 14:43:02 +0700

    implement cache api to project

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,6 +2,7 @@ const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/',
   DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'RestoKan-V1',
 };
 
 export default CONFIG;
